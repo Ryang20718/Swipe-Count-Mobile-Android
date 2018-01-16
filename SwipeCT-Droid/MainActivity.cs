a868db2fb7b330f566062d8f83a0af80f093854d
Ryan Yang <Ryanyang99@hotmail.com>
2018-01-15 16:17:40 -0800

    First Commit

---
+++ SwipeCT-Droid/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Webkit;
+
+namespace SwipeCTDroid
+{
+    [Activity(Label = "SwipeCTDroid", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+
+
+            SetContentView(Resource.Layout.Main);
+            ActionBar.Hide(); 
+            WebView webView = FindViewById<WebView>(Resource.Id.LocalWebView);
+            webView.SetWebViewClient(new WebViewClient()); // stops request going to Web Browser
+            webView.Settings.JavaScriptEnabled = true;
+            webView.LoadUrl("http://ryanyang.me/SwipeCount/");
+
+        }
+    }
+}
+
